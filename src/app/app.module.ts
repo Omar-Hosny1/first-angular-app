39661fd64d83b4d9bcbd6721012f30c85fd643e2
= <hosnyomar646@gmail.com>
2022-10-05 15:43:28 +0200

    SCH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServersComponent } from './servers/servers.component';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
 import { AlertComponent } from './alert/alert.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,7 @@ import { AlertComponent } from './alert/alert.component';
     SuccessComponent,
     AlertComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,16 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: '[app-servers]',
   selector: '.app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <h3>Hello</h3>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
-
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
+  serverName: string = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
   ngOnInit(): void {}
 }
